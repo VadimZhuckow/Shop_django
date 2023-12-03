2cdb798cd520a8b96d4ab4c7e7a19f5c922d1cfe
Egor <egorobidin@inbox.ru>
2023-12-03 23:09:03 +0300

    Обновление 1-ой ЛР (доработка недочётов)

--- files/lab1/weather_api.py
+++ files/lab1/weather_api.py
@@ -28,21 +28,23 @@ def current_weather(lat, lon):
     Описание функции, входных и выходных переменных
     """
     token = 'Ваш токен'  # Вставить ваш токен
-    url = f"https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}"
+    url = f"https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}"  # Если вдруг используете тариф «Погода на вашем сайте»
+    # то вместо forecast используйте informers. url = f"https://api.weather.yandex.ru/v2/informers?lat={lat}&lon={lon}"
     headers = {"X-Yandex-API-Key": f"{token}"}
     response = requests.get(url, headers=headers)
     data = response.json()
 
+    # Данная реализация приведена для тарифа «Тестовый», если у вас Тариф «Погода на вашем сайте», то закомментируйте пару строк указанных ниже
     result = {
-        'city': data['geo_object']['locality']['name'],
-        'time': datetime.fromtimestamp(data['fact']['uptime']).strftime("%H:%M"),
+        'city': data['geo_object']['locality']['name'],  # Если используете Тариф «Погода на вашем сайте», то закомментируйте эту строку
+        'time': datetime.fromtimestamp(data['fact']['uptime']).strftime("%H:%M"),  # Если используете Тариф «Погода на вашем сайте», то закомментируйте эту строку
         'temp': 'реализация',  # TODO Реализовать вычисление температуры из данных полученных от API
         'feels_like_temp': 'реализация',  # TODO Реализовать вычисление ощущаемой температуры из данных полученных от API
         'pressure': 'реализация',  # TODO Реализовать вычисление давления из данных полученных от API
         'humidity': 'реализация',  # TODO Реализовать вычисление влажности из данных полученных от API
         'wind_speed': 'реализация',  # TODO Реализовать вычисление скорости ветра из данных полученных от API
         'wind_gust': 'реализация',  # TODO Реализовать вычисление скорости порывов ветка из данных полученных от API
-        'wind_dir': DIRECTION_TRANSFORM.get(data['fact']['wind_dir']),
+        'wind_dir': DIRECTION_TRANSFORM.get(data['fact']['wind_dir']),  # Если используете Тариф «Погода на вашем сайте», то закомментируйте эту строку
     }
     return result
 
