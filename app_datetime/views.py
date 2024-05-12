28521d5c883b80209f6bbd6d44c1815103c38c83
Vadim Zhukov <apexknight@yandex.ru>
2024-05-12 12:17:35 +0300

    task 2 - refactor

--- app_datetime/views.py
+++ app_datetime/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
+import time
 from datetime import datetime
 
 
 def date(request: HttpRequest) -> HttpResponse:
     if request.method == 'GET':
-        data = datetime.now()
-        return HttpResponse(data)
+        data = datetime.today()
+        return HttpResponse(data.strftime('%d %m - %H:%M'))
 
 # Create your views here.
