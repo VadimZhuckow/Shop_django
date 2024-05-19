43479cebb884ff8bb17794ff2aeb912a78367dfa
Vadim Zhukov <apexknight@yandex.ru>
2024-05-19 14:01:43 +0300

    task 3 - Receive the page quickly

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import products_view, shop_view
+from .views import products_view, shop_view, products_page_view
 
 urlpatterns = [
-    path('product/', products_view),
     path('', shop_view),
+    path('product/', products_view),
+    path('product/<slug:page>.html', products_page_view),
 ]
--- store/views.py
+++ store/views.py
@@ -4,28 +4,7 @@ from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseNot
 
 from django.http import HttpRequest, HttpResponse, JsonResponse
 
-
 from .models import DATABASE
-
-
-
-
-def products_view(request: HttpRequest) -> HttpResponse:
-    if request.method == 'GET':
-        id_cart = request.GET.get('id')
-        if id_cart in DATABASE.keys():
-            return JsonResponse(DATABASE.get(id_cart),
-                                json_dumps_params={
-                                    "indent": 4,
-                                    "ensure_ascii": False
-                                })
-        if not id_cart:
-            return JsonResponse(DATABASE,
-                                json_dumps_params={
-                                    "indent": 4,
-                                    "ensure_ascii": False})
-        if id_cart not in DATABASE.keys():
-            return HttpResponse('Такого товара нет в базе')
 
 
 def shop_view(request: HttpRequest) -> HttpResponse:
@@ -33,3 +12,36 @@ def shop_view(request: HttpRequest) -> HttpResponse:
         with open('store/shop.html', encoding='utf-8') as f:
             data = f.read()
         return HttpResponse(data)
+
+
+def products_view(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        id_cart = request.GET.get('id')
+        if id_cart in DATABASE.keys():
+            return JsonResponse(DATABASE.get(id_cart),
+
+                                json_dumps_params={
+                                    "indent": 4,
+                                    "ensure_ascii": False
+                                })
+        if not id_cart:
+            return JsonResponse(DATABASE,
+
+                                json_dumps_params={
+                                    "indent": 4,
+                                    "ensure_ascii": False})
+        if id_cart not in DATABASE.keys():
+            return HttpResponseNotFound('Такого товара нет в базе')
+
+
+def products_page_view(request: HttpRequest, page: str) -> HttpResponse:
+    if request.method == "GET":
+        for data in DATABASE.values():
+            if data["html"] == page:
+                with open(f"store/products/{page}.html", encoding="utf-8") as f:
+                    page = f.read()
+
+                return HttpResponse(page)
+
+
+
